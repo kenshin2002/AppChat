f5340f483c8813b8cea022ae665cc0559e77bbb1
kenshin2002 <hoang.bt182002@gmail.com>
2024-09-26 10:36:16 +0700

    Initial project

---
+++ AppChat/Views/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using AppChat.ViewModels;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace AppChat.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainViewModel ViewModel
+    {
+        get;
+    }
+
+    public MainPage()
+    {
+        ViewModel = App.GetService<MainViewModel>();
+        InitializeComponent();
+    }
+    private void OnEnterClicked(object sender, RoutedEventArgs e)
+    {
+        string userName = NameTextBox.Text;
+        if (!string.IsNullOrEmpty(userName))
+        {
+            // Điều hướng đến trang chat với tên người dùng
+            Frame.Navigate(typeof(ChatPage), userName);
+        }
+    }
+}
